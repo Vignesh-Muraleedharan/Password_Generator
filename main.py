79344436e332cbee351638106636e7c4801d3bf2
Vignesh Muraleedharan <76657109+Vignesh-Muraleedharan@users.noreply.github.com>
2024-03-14 20:02:56 +0530

    Testing

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import streamlit as st
 import random
 import string
-st.write("""
-         Password Generator
-         """
-         )
 
 def generate_password(length, special_char):
-    chr = string.ascii_letters + string.digits
-    password = ''
-    if (special_char == 'Yes'):
-        chr += string.punctuation
-    for i in range(length):
-        password += random.choice(chr)
+    characters = string.ascii_letters + string.digits
+    if special_char == 'Yes':
+        characters += string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-#User Input
-length = st.slider("Password Length", min_value=6, max_value=20, value=10, step=1)
-special_char = st.radio("Contain special characters?", ("Yes", "No"))
+def main():
+    st.title("Password Generator")
 
-#Generate and print the password
-password = generate_password(length, special_char)
-st.write("Generated Password: ", password)
+    # User Input
+    length = st.slider("Password Length", min_value=6, max_value=20, value=10, step=1)
+    special_char = st.radio("Contain special characters?", ("Yes", "No"))
+
+    # Generate and display the password
+    password = generate_password(length, special_char)
+    st.write("Generated Password:", password)
+
+if __name__ == '__main__':
+    main()
